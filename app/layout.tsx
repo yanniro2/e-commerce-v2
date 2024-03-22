64f43201be4737b0a5346aa6e3614d4d2af2d615
Niroyan Kugaruban <k.niroyan@gmail.com>
2024-03-22 12:35:47 -0400

    favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
         <CartProvider>{children}</CartProvider>
       </body>
