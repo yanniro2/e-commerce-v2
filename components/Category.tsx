c25326ae7ad943c3022e4892dc3f40c5ec862e03
Niroyan Kugaruban <k.niroyan@gmail.com>
2024-03-22 14:28:26 -0400

    fix category components

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,28 +1,26 @@
 
-import { createReader } from "@keystatic/core/reader";
-import keystaticConfig from "../keystatic.config";
-import Image from 'next/image';
-import React from 'react'
+
+import Image from "next/image";
+import React from "react";
 import Link from "next/link";
-const reader = createReader(process.cwd(), keystaticConfig);
+import data from "../data/category.json";
 
 export default async function Category() {
-  const category = await reader.collections.category.all();
   return (
     <section className="w-screen h-[70vh] bg-milkWhite">
       <div className="container mx-auto py-[5rem] h-full flex items-center justify-between gap-[2rem]">
-        {category.map((category) => (
+        {data.categories.map((category) => (
           <div
-            key={category.slug}
+            key={category.title}
             className="w-1/3 h-2/3  bg-gray rounded-xl relative flex items-center justify-center flex-col pt-[5rem]">
             <Image
-              src={category.entry.Desktop ?? ""}
+              src={`/assets/shared/desktop/${category.img}` ?? ""}
               alt="category img"
               className=" absolute top-0 left-1/2 -translate-y-1/4 w-full h-full -translate-x-1/2 object-contain"
               width={500}
               height={500}
             />
-            <h1 className="h6 mt-[3rem]">{category.entry.title}</h1>
+            <h1 className="h6 mt-[3rem]">{category.title}</h1>
             <Link href={`/${category.slug}`} className="btn btn-3">
               Shop &#8250;
             </Link>
